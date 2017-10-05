f55d0eb9c9ba93541d6b07e006680438e4022824
Alberto Silva <asilvadesigns@gmail.com>
2017-10-05 16:54:56 -0400

    working live refresh with jekyll, no scss or js yet

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,37 @@
 import gulp from 'gulp';
+import cp from 'child_process';
+import sync from 'browser-sync';
 
-const Message = (done) => {
-  console.log('hello world!');
-  done();
+const JEKYLL = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+const PATHS = {
+  jekyll: {
+    src: ['./index.html', './_includes/*', './_layouts/*', './_pages/*', './_posts/*', './_projects'],
+    dest: './_site/'
+  }
 }
 
-gulp.task('default', gulp.series(Message));
+const Build = (done) => {
+  return cp.spawn(JEKYLL , ['build'], {stdio: 'inherit'}).on('close', done);
+}
+
+const Reload = () => {
+  sync.reload();
+}
+
+const Serve = () => {
+  sync.init({
+    server: PATHS.jekyll.dest,
+    notify: false
+  });
+}
+
+const Watch = () => {
+  gulp.watch(PATHS.jekyll.src).on('change', gulp.series(Build, Reload));
+}
+
+gulp.task('default',
+  gulp.series(
+    Build,
+    gulp.parallel(Serve, Watch)
+  )
+);
